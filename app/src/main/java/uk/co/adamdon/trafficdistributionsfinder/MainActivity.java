870c3bb07c3008ad6400653abca8d54d5ae090f3
adamdon <mail@adamdon.co.uk>
2022-02-03 19:11:43 +0000

    Initial commit

---
+++ app/src/main/java/uk/co/adamdon/trafficdistributionsfinder/MainActivity.java
@@ -0,0 +1,16 @@
+package uk.co.adamdon.trafficdistributionsfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
