af513d32427da3c3348c0c746d20e0daf25cb42b
adamdon <mail@adamdon.co.uk>
2022-02-15 19:29:23 +0000

    Started livedata listview setup

--- app/src/main/java/uk/co/adamdon/trafficdistributionsfinder/ui/viewmodels/CurrentViewModel.java
+++ app/src/main/java/uk/co/adamdon/trafficdistributionsfinder/ui/viewmodels/CurrentViewModel.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -11,6 +12,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
 import uk.co.adamdon.trafficdistributionsfinder.business.Config;
 import uk.co.adamdon.trafficdistributionsfinder.models.CurrentIncidentModel;
@@ -21,8 +23,9 @@ import uk.co.adamdon.trafficdistributionsfinder.utilities.DataFetcher;
 
 public class CurrentViewModel extends AbstractViewModel
 {
+    private MutableLiveData<List<CurrentIncidentModel>> currentIncidentListLiveData;
 
-    String fullResultString = null;
+//    String fullResultString = null;
 
 
 
@@ -45,8 +48,10 @@ public class CurrentViewModel extends AbstractViewModel
     }
 
 
-
-    public void setValue(Object test)
+    //TODO add ListView UI elements
+    //TODO create listView array Adapter
+    //TODO refactor setValue out
+    public void setValue(Object test) //refactor this out
     {
         Log.d("TAG", "DID THIS WORK" + Thread.currentThread().getName());
         Log.d("TAG", "setValue: test value update in viewmodel ");
@@ -55,78 +60,96 @@ public class CurrentViewModel extends AbstractViewModel
 
         ArrayList<CurrentIncidentModel> currentIncidentList;
         currentIncidentList = XmlToCurrentInstancesList.getInstance().parse(test.toString());
+        setCurrentIncidentListLiveData(currentIncidentList);
         Log.d("TAG List",  currentIncidentList.toString());
+        Log.d("TAG List",  String.valueOf(currentIncidentList.size()));
 
     }
 
-
-
-
-    private class Task implements Runnable
+    public MutableLiveData<List<CurrentIncidentModel>> getCurrentIncidentListLiveData()
     {
-        private String urlString;
-
-        public Task(String urlString)
+        if (currentIncidentListLiveData == null)
         {
-            this.urlString = urlString;
+            currentIncidentListLiveData = new MutableLiveData<>();
         }
-        @Override
-        public void run()
-        {
-
-            URL urlObject;
-            URLConnection urlConnection;
-
-            InputStream inputStream;
-            InputStreamReader inputStreamReader;
-            BufferedReader bufferedReader;
-
-            String currentLineString = "";
-
-
-
-            try
-            {
-                urlObject = new URL(urlString);
-                urlConnection = urlObject.openConnection();
-
-                inputStream = urlConnection.getInputStream();
-                inputStreamReader = new InputStreamReader(inputStream);
-                bufferedReader = new BufferedReader(inputStreamReader);
-
-
-                currentLineString = bufferedReader.readLine(); // read first line
-                while(currentLineString != null)
-                {
-                    fullResultString = fullResultString + currentLineString;
-                    fullResultString = fullResultString + System.lineSeparator();
-
-                    Log.d("output",currentLineString);
-
-                    currentLineString = bufferedReader.readLine(); //read next line
-                }
-                bufferedReader.close();
-
-            }
-            catch (Exception e)
-            {
-                Log.d("testa", "What's going on here?");
-                Log.e("testb", e.getMessage());
-                Log.d("testc", "ioexception in run");
-            }
-
-
-            app.getUiController().getCurrentActivity().runOnUiThread(new Runnable()
-            {
-                public void run() {
-                    Log.d("UI thread", "I am the UI thread");
-                    Log.d("UI thread", fullResultString);
-                }
-            });
-
-        }
-
+        return currentIncidentListLiveData;
     }
+
+    public void setCurrentIncidentListLiveData(ArrayList<CurrentIncidentModel> currentIncidentList)
+    {
+        currentIncidentListLiveData.setValue(currentIncidentList);
+    }
+
+
+
+
+
+
+    //    private class Task implements Runnable
+//    {
+//        private String urlString;
+//
+//        public Task(String urlString)
+//        {
+//            this.urlString = urlString;
+//        }
+//        @Override
+//        public void run()
+//        {
+//
+//            URL urlObject;
+//            URLConnection urlConnection;
+//
+//            InputStream inputStream;
+//            InputStreamReader inputStreamReader;
+//            BufferedReader bufferedReader;
+//
+//            String currentLineString = "";
+//
+//
+//
+//            try
+//            {
+//                urlObject = new URL(urlString);
+//                urlConnection = urlObject.openConnection();
+//
+//                inputStream = urlConnection.getInputStream();
+//                inputStreamReader = new InputStreamReader(inputStream);
+//                bufferedReader = new BufferedReader(inputStreamReader);
+//
+//
+//                currentLineString = bufferedReader.readLine(); // read first line
+//                while(currentLineString != null)
+//                {
+//                    fullResultString = fullResultString + currentLineString;
+//                    fullResultString = fullResultString + System.lineSeparator();
+//
+//                    Log.d("output",currentLineString);
+//
+//                    currentLineString = bufferedReader.readLine(); //read next line
+//                }
+//                bufferedReader.close();
+//
+//            }
+//            catch (Exception e)
+//            {
+//                Log.d("testa", "What's going on here?");
+//                Log.e("testb", e.getMessage());
+//                Log.d("testc", "ioexception in run");
+//            }
+//
+//
+//            app.getUiController().getCurrentActivity().runOnUiThread(new Runnable()
+//            {
+//                public void run() {
+//                    Log.d("UI thread", "I am the UI thread");
+//                    Log.d("UI thread", fullResultString);
+//                }
+//            });
+//
+//        }
+//
+//    }
 
 
 }
